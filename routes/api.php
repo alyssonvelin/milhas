15d0c2586d3f022f32be8ee8cd7d83c7d8a1d1c6
Camargos Rezende <a.camargos.rezende@accenture.com>
2021-03-02 14:56:18 -0300

    atualizando

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\{
-    ApimilhasController
+    ApiemprestaController
 };
 
 /*
@@ -21,5 +21,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/milhas',[ApimilhasController::class,'index']);
+Route::get('/emprestainstituicoes',[ApiemprestaController::class,'instituicoes'])->name('empresta.instituicoes');
+Route::get('/emprestaconvenios',[ApiemprestaController::class,'convenios'])->name('empresta.convenios');
+Route::post('/empresta',[ApiemprestaController::class,'index'])->name('empresta.index');
 
