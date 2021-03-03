39d74644077d8fe0ed78e6ad23158ba6d6e7a2a6
Camargos Rezende <a.camargos.rezende@accenture.com>
2021-03-03 14:23:28 -0300

    atualizando

--- app/Models/Apiempresta.php
+++ app/Models/Apiempresta.php
@@ -12,14 +12,15 @@ class Apiempresta extends Model
 
     /**
      * Get file content
-     * @param array $data
+     * @param string $name
+     * @param string $path
      * @return bool
      */
     public function getFile($name,$path)
     {
         try
         {
-            return file_get_contents(storage_path().$path.$name);
+            return file_get_contents($path.'/'.$name);
         }
         catch(Exception $e)
         {
